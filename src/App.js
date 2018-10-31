f2f08434452e3c8e98dde7f25ad348b358cdeacb
Frederik Funke <frederik-funke@gmx.de>
2018-10-31 22:04:18 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        A new quote for today!
+      </div>
+    )
+  }
+}
+
+class Quote extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+class Buttons extends Component {
+  render() {
+    return (
+      <div className="buttons">
+        <button>Next</button>
+        <button>Tweet</button>
+      </div>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header />
+        <Quote />
+        <Buttons />
+
+        {/*
+        App container
+          header
+          quote
+            text
+            author
+          buttons
+            next
+            tweet
+        */}
+
       </div>
     );
   }
