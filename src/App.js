f453eb31686aa2917fb5f5875d6089e328473a1d
Frederik Funke <frederik-funke@gmx.de>
2018-11-01 01:42:53 +0100

    package.json basic layout tests working api call

--- src/App.js
+++ src/App.js
@@ -1,56 +1,85 @@
 import React, { Component } from 'react';
 import './App.css';
 
-class Header extends Component {
-  render() {
-    return (
-      <div className="header">
-        A new quote for today!
-      </div>
-    )
-  }
+/*
+"https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
+
+response:
+  text: [0].content
+  author: [0].title
+
+`https://twitter.com/intent/tweet?text=${encdeURIComponent(textToTweet)}`
+*/
+
+const Header = () => {
+  return (
+    <div className="header">
+      A new quote for today!
+    </div>
+  )
 }
 
 class Quote extends Component {
   render() {
-    return (
-      <div>
+    const url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          text: data[0].content,
+          author: data[0].title
+        })
         
+        console.log(data[0])
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    return (
+      <div className="quote-container">
+        <div id="text">{this.props.text}</div>
+        <div id="author">{this.props.author}</div>
       </div>
     )
   }
 }
 
-class Buttons extends Component {
-  render() {
-    return (
-      <div className="buttons">
-        <button>Next</button>
-        <button>Tweet</button>
-      </div>
-    )
-  }
+const Buttons = () => {
+  return (
+    <div className="buttons-container">
+      <button id="new-quote">Next</button>
+      <a id="tweet-quote" href="">Tweet</a>
+    </div>
+  )
 }
 
 class App extends Component {
+  state = {
+    text: "aaa",
+    author: "bbb"
+  }
   render() {
     return (
       <div className="App">
-        <Header />
-        <Quote />
-        <Buttons />
+        <div id="quote-box">
+          <Header />
+          <Quote text={this.state.text} author={this.state.author} />
+          <Buttons />
 
-        {/*
-        App container
-          header
-          quote
-            text
-            author
-          buttons
-            next
-            tweet
-        */}
+          {/*
+          App container
+            header
+            quote
+              text
+              author
+            buttons
+              next
+              tweet
+          */}
 
+        </div>
       </div>
     );
   }
