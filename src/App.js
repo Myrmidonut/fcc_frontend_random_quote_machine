c1210c154571c52ca77fa919b4c46292497d1695
Frederik Funke <frederik-funke@gmx.de>
2018-11-02 01:14:41 +0100

    working prototype

--- src/App.js
+++ src/App.js
@@ -21,64 +21,61 @@ const Header = () => {
 
 class Quote extends Component {
   render() {
-    const url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=";
-
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({
-          text: data[0].content,
-          author: data[0].title
-        })
-        
-        console.log(data[0])
-      })
-      .catch(error => {
-        console.log(error);
-      });
-
     return (
       <div className="quote-container">
-        <div id="text">{this.props.text}</div>
+        <div id="text" dangerouslySetInnerHTML={{ __html: this.props.text }}></div>
         <div id="author">{this.props.author}</div>
       </div>
     )
   }
 }
 
-const Buttons = () => {
+const Buttons = (props) => {
   return (
     <div className="buttons-container">
-      <button id="new-quote">Next</button>
-      <a id="tweet-quote" href="">Tweet</a>
+      <button id="new-quote" onClick={props.getQuote}>Next</button>
+      <a id="tweet-quote" href={`https://twitter.com/intent/tweet?text=${props.plainText}%20-%20${props.author}`} target="_blank" rel="noopener noreferrer">Tweet</a>
     </div>
   )
 }
 
 class App extends Component {
   state = {
-    text: "aaa",
-    author: "bbb"
+    text: "",
+    author: "",
+    plainText: ""
   }
+
+  getQuote = () => {
+    const url = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1"; //&callback=";
+
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({
+        text: data[0].content,
+        author: data[0].title
+      })
+    })
+    .then(() => {
+      this.setState({
+        plainText: document.getElementById("text").innerText
+      })
+    })
+    .catch(error => console.log(error))
+  }
+
+  componentWillMount() {
+    this.getQuote();
+  }
+
   render() {
     return (
       <div className="App">
         <div id="quote-box">
           <Header />
           <Quote text={this.state.text} author={this.state.author} />
-          <Buttons />
-
-          {/*
-          App container
-            header
-            quote
-              text
-              author
-            buttons
-              next
-              tweet
-          */}
-
+          <Buttons getQuote={this.getQuote} plainText={this.state.plainText} author={this.state.author} />
         </div>
       </div>
     );
